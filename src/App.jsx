0f82cc24194bd4cfd3ddcc20bd9677467470f521
lup1n <luckg3030@gmail.com>
2023-05-31 14:07:41 -0300

    mejoras

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Papa from "papaparse";
 import * as XLSX from "xlsx";
+import axios from "axios";
 
 function App() {
   const [parsedData, setParsedData] = useState([]);
@@ -38,58 +39,72 @@ function App() {
     Papa.parse(file, {
       complete: function (results) {
         console.log(results.data);
-        // Aquí puedes realizar la lógica para extraer la información y almacenarla en algún lugar
         const parsedData = extractDataFromCSV(results.data);
         setParsedData(parsedData);
       },
     });
   };
 
-  const parseTXT = (file) => {
+  const parseTXT = async (file) => {
     const reader = new FileReader();
-    reader.onload = function (event) {
+    reader.onload = async function (event) {
       const contents = event.target.result;
-      const parsedData = extractDataFromTXT(contents);
+      const parsedData = await extractDataFromTXT(contents);
       setParsedData(parsedData);
     };
     reader.readAsText(file);
   };
 
   const extractDataFromCSV = (data) => {
-    // Aquí puedes implementar la lógica para extraer los datos del archivo CSV
     return data;
   };
 
-  const extractDataFromTXT = (contents) => {
-    const regex = /"ipAudit"\s*:\s*{\s*"accountId"\s*:\s*"([^"]+)"\s*,\s*"createdAt"\s*:\s*"([^"]+)"\s*,\s*"loginIp"\s*:\s*"([^"]+)"\s*}/gs;
-    const matches = contents.match(regex);
+  const extractDataFromTXT = async (contents) => {
+    const regex =
+      /"accountId"\s*:\s*"([^"]+)"\s*,\s*"createdAt"\s*:\s*"([^"]+)"\s*,\s*"loginIp"\s*:\s*"([^"]+)"\s*}/gs;
+    const matches = Array.from(contents.matchAll(regex));
     const parsedData = [];
-  
-    if (matches) {
-      for (const match of matches) {
+
+    for (const match of matches) {
+      try {
+        const accountId = match[1];
+        const createdAt = match[2];
+        const loginIp = match[3];
+
+        let org = "";
         try {
-          const matchObj = JSON.parse(`{${match}}`);
-          const accountId = matchObj.ipAudit.accountId;
-          const createdAt = matchObj.ipAudit.createdAt;
-          const loginIp = matchObj.ipAudit.loginIp;
-          parsedData.push({ accountId, createdAt, loginIp });
+          const access_token = "814ee26772f463";
+          const response = await axios.get(
+            `https://ipinfo.io/${loginIp}/org?token=${access_token}`
+          );
+          org = response.data;
         } catch (error) {
-          console.error("Error al analizar el objeto JSON:", error);
+          console.error("Error al obtener la información de la IP:", error);
         }
+
+        parsedData.push({ accountId, createdAt, loginIp, org });
+      } catch (error) {
+        console.error("Error al analizar el objeto JSON:", error);
       }
     }
 
     return parsedData;
   };
-  
 
   const getFileExtension = (filename) => {
     return filename.slice(((filename.lastIndexOf(".") - 1) >>> 0) + 2);
   };
 
   const exportToExcel = () => {
+    if (parsedData.length === 0) {
+      console.log("No hay datos para exportar.");
+      return;
+    }
+
     const workbook = XLSX.utils.book_new();
-    const worksheet = XLSX.utils.json_to_sheet(parsedData);
+    const worksheet = XLSX.utils.json_to_sheet(parsedData, {
+      header: ["accountId", "createdAt", "loginIp", "org"],
+    });
 
     XLSX.utils.book_append_sheet(workbook, worksheet, "Datos procesados");
 
@@ -112,6 +127,7 @@ function App() {
 
   return (
     <div className="bg-principal w-screen h-screen flex items-center justify-center">
+      <h1 className="text-4xl mb-8 absolute top-0 transform -translate-x-1/2 left-1/2" style={{ color: 'white' }}>Reportes Twitter</h1>
       <div className="w-96 h-96 bg-white rounded-xl shadow-md p-8">
         <h1 className="text-3xl mb-4">Cargar Archivo</h1>
         <button
@@ -121,23 +137,6 @@ function App() {
           Exportar a Excel
         </button>
         <FileUploader onFileUpload={handleFileUpload} />
-        <div className="mt-4">
-          <h2 className="text-xl mb-2">Datos procesados:</h2>
-          {parsedData.length > 0 ? (
-            <ul>
-              {parsedData.map((data, index) => (
-                <li key={index}>
-                  <p>
-                    Account ID: {data.accountId}, Created At: {data.createdAt},{" "}
-                    Login IP: {data.loginIp}
-                  </p>
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p>No hay datos procesados</p>
-          )}
-        </div>
       </div>
     </div>
   );
